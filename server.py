8fa8332f5937e0b4ca02ec72d45718b875c38ad9
Matt Arnold <matt.arnd@gmail.com>
2022-06-12 15:03:35 -0500

    first upload of assignment

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
-from re import X
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = "secret"
 
+list_of_users = [
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
 @app.route( "/" )
 @app.route( "/home" )
 def show_8_by_8_checkerboard():
-    return render_template( "index.html" )
+    return render_template( "index.html", list_of_users = list_of_users )
 
 @app.route( "/<x>" )
 def show_custom_rows_checkerboard(x):
